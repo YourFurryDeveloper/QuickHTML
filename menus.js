5732c76f5f24d23c7eda4085a3fcb06570f41344
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-04-24 13:44:56 -0400

    Update menus.js :3

--- menus.js
+++ menus.js
@@ -1,8 +1,12 @@
 function openToolbarMenu(menu) {
     if (menu === "file") {
+        document.getElementById("toolmenu").style.display = "none";
         document.getElementById("filemenu").style.display = "unset";
     } else if (menu === "edit") {
         document.getElementById("infoBox").setAttribute("open", "true");
+    } else if (menu === "tools") {
+        document.getElementById("filemenu").style.display = "none";
+        document.getElementById("toolmenu").style.display = "unset";
     } else if (menu === "info") {
         document.getElementById("infoBox").setAttribute("open", "true");
     }
@@ -13,6 +17,8 @@ function closeToolbarMenu(menu) {
         document.getElementById("filemenu").style.display = "none";
     } else if (menu === "edit") {
         document.getElementById("infoBox").removeAttribute("open");
+    } else if (menu === "tools") {
+        document.getElementById("toolmenu").style.display = "none";
     } else if (menu === "info") {
         document.getElementById("infoBox").removeAttribute("open");
     }
